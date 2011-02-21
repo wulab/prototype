9c631f5dbc82b303718876399ded05c86846b933
Weera Wu <weera.or@wu.ac.th>
2011-02-21 18:07:20 +0700

    Integrated with HTML5 Boilerplate and the 1KB CSS Grid.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title
+    base_title = "Project Management App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
