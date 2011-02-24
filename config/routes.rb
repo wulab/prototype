77859cdb3d0836bdb67e79eef9e156826488bfdc
Weera Wu <weera.or@wu.ac.th>
2011-02-24 15:08:21 +0700

    Added rails routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Prototype::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/about"
-
+  match '/about', :to => 'pages#about'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +50,7 @@ Prototype::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
