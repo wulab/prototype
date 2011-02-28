ef85628b2bcd0590f73186a33808f529e64eac89
Weera Wu <weera.or@wu.ac.th>
2011-02-28 14:21:24 +0700

    Defined a logo helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,14 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def logo
+    text_logo = "Company name"
+    if @project.nil?
+      content_tag(:h1, link_to(text_logo, root_path), :id => "logo")
+    else
+      @project.name
+    end
+  end
 
 end
