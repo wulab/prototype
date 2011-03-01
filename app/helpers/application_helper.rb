179f9f11c46d9917d1b3896588ba31a94a934dde
Weera Wu <weera.or@wu.ac.th>
2011-03-01 17:23:40 +0700

    Rewrited error_messages_for() method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,11 +22,18 @@ module ApplicationHelper
   end
 
   def error_messages_for(object, attribute)
-    if object.errors[attribute].any?
-      messages = object.errors[attribute].collect do |value|
+    return nil if object.errors[attribute].empty?
+    
+    messages = object.errors[attribute].collect do |value|
                    "#{attribute.to_s.titlecase} #{value}."
                  end.join('<br />')
-      "<p class=\"inline-errors\">#{messages}</p>".html_safe
-    end
+    
+    html = <<-HTML
+    <p class="inline-errors">
+      #{messages}
+    </p>
+    HTML
+    
+    html.html_safe
   end
 end
